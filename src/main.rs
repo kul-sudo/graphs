3489c13ad53ad484e5d40bd4abca1999ceb755d5
kul-sudo <ulyanov.kirill@protonmail.com>
2024-12-20 20:46:24 +0300

    demonstration added

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,26 @@
-use rand::{rngs::StdRng, seq::IteratorRandom, SeedableRng};
+use ::rand::random;
+use macroquad::prelude::*;
 use serde::{Deserialize, Serialize};
-use std::{cmp::Ordering, collections::HashSet, process::exit, time::Instant};
+use std::{f32::consts::PI, process::exit, time::Instant};
 
-const NODES_N: usize = 10;
-const EDGES_N: usize = 15;
-const GRAPHS_N: usize = 100_000;
+const NODES_N: usize = 12;
+static MAX_EDGES_N: usize = (NODES_N * (NODES_N - 1)) / 2;
+static EDGES_THRESHOLD: usize = (MAX_EDGES_N as f32 * 0.5) as usize;
+
+const GRAPHS_N: usize = 500;
+
+const GRAPH_RADIUS: f32 = 500.0;
+const NODE_RADIUS: f32 = 10.0;
 
 #[derive(Serialize, Deserialize)]
 struct Graph {
     nodes_n: usize,
-    edges_n: usize,
     edges: Vec<Vec<bool>>,
 }
 
 #[derive(Serialize, Deserialize)]
 struct Info {
     nodes_n: usize,
-    edges_n: usize,
     graphs_n: usize,
 }
 
@@ -26,28 +30,40 @@ struct Content {
     graphs: Vec<Vec<Vec<bool>>>,
 }
 
+#[derive(PartialEq, Clone, Copy)]
+enum GraphKind {
+    Hamiltonian,
+    NonHamiltonian,
+}
+
 impl Graph {
-    fn path_exists(&self, nodes: HashSet<usize>, current_node: usize) -> bool {
-        if nodes.is_empty() {
-            return self.edges[current_node][0];
+    fn get_path(&self, mut path: Vec<usize>, current_node: usize) -> Vec<usize> {
+        if current_node == 0 && path.len() == NODES_N {
+            path.push(0);
+            return path;
         }
 
-        for node in &nodes {
-            if self.edges[current_node][*node] {
-                let mut new_set = nodes.clone();
-                new_set.remove(node);
+        if path.contains(&current_node) {
+            return vec![];
+        }
 
-                if self.path_exists(new_set, *node) {
-                    return true;
+        path.push(current_node);
+
+        for node in 0..NODES_N {
+            if self.edges[current_node][node] {
+                let path = self.get_path(path.clone(), node);
+
+                if !path.is_empty() {
+                    return path;
                 }
             }
         }
 
-        false
+        vec![]
     }
 
-    fn is_hamiltonian(&self) -> bool {
-        self.path_exists((1..self.nodes_n).collect::<HashSet<_>>(), 0)
+    fn get_cycle(&self) -> Vec<usize> {
+        self.get_path(Vec::with_capacity(NODES_N), 0)
     }
 
     fn safety(&self) {
@@ -56,27 +72,12 @@ impl Graph {
             eprintln!("The number of nodes can't be lower than 3.");
             exit(1);
         }
-
-        if self.edges_n < self.nodes_n {
-            eprintln!("There can't be that few edges.");
-            exit(1);
-        }
-
-        // en.wikipedia.org/wiki/Combination
-        if self.edges_n > (self.nodes_n * (self.nodes_n - 1)) / 2 {
-            eprintln!("There can't be that many edges.");
-            exit(1);
-        }
     }
 
-    fn new(nodes_n: usize, edges_n: usize) -> Self {
+    fn new(nodes_n: usize) -> Self {
         let edges = vec![vec![false; nodes_n]; nodes_n];
 
-        Self {
-            nodes_n,
-            edges_n,
-            edges,
-        }
+        Self { nodes_n, edges }
     }
 
     fn manage_edge(&mut self, i: usize, j: usize, value: bool) {
@@ -84,111 +85,167 @@ impl Graph {
         self.edges[j][i] = value;
     }
 
-    fn generate(&mut self, rng: &mut StdRng) {
-        let mut edges_n;
+    fn generate(&mut self) {
+        for i in 0..self.nodes_n {
+            for j in i + 1..self.nodes_n {
+                self.manage_edge(i, j, random::<bool>());
+            }
+        }
+    }
 
+    fn generate_with_given_kind(&mut self, kind: GraphKind) {
         loop {
-            // Make 2 random edges out of each node
-            for i in 0..self.nodes_n {
-                let other_edges = (0..self.nodes_n).filter(|j| *j != i);
-                let random_pair = other_edges.choose_multiple(rng, 2);
+            self.generate();
 
-                self.manage_edge(i, random_pair[0], true);
-                self.manage_edge(i, random_pair[1], true);
+            match kind {
+                GraphKind::Hamiltonian => {
+                    if self
+                        .edges
+                        .iter()
+                        .flatten()
+                        .map(|x| *x as usize)
+                        .sum::<usize>()
+                        / 2
+                        > EDGES_THRESHOLD
+                    {
+                        continue;
+                    }
+                }
+                GraphKind::NonHamiltonian => {
+                    if (0..NODES_N)
+                        .any(|i| self.edges[i].iter().map(|x| *x as usize).sum::<usize>() < 2)
+                    {
+                        continue;
+                    }
+                }
             }
 
-            edges_n = self
-                .edges
-                .iter()
-                .flatten()
-                .map(|y| *y as usize)
-                .sum::<usize>()
-                / 2;
-
-            match edges_n.cmp(&self.edges_n) {
-                Ordering::Equal => {
-                    return;
-                }
-                Ordering::Less => {
-                    let mut non_existent = Vec::new();
-
-                    for i in 0..self.nodes_n {
-                        for j in i + 1..self.nodes_n {
-                            if !self.edges[i][j] {
-                                non_existent.push((i, j));
-                            }
-                        }
-                    }
-
-                    let random = non_existent
-                        .iter()
-                        .choose_multiple(rng, self.edges_n - edges_n);
-
-                    for (i, j) in random {
-                        self.manage_edge(*i, *j, true);
-                    }
-
-                    return;
-                }
-                Ordering::Greater => {
-                    // We could theoretically remove the unneeded edges,
-                    // but it's easier to start over
-                    *self = Graph::new(self.nodes_n, self.edges_n);
-                }
+            if self.get_cycle().is_empty() != (kind == GraphKind::Hamiltonian) {
+                break;
+            } else {
+                *self = Graph::new(NODES_N);
             }
         }
     }
 }
 
-fn main() {
-    let mut rng = StdRng::from_rng(&mut rand::thread_rng()).unwrap();
+enum Mode {
+    Demonstration,
+    Generation,
+}
+const MODE: Mode = Mode::Demonstration;
 
-    //let mut hamiltonian_n = 0;
+#[macroquad::main("BasicShapes")]
+async fn main() {
+    match MODE {
+        Mode::Demonstration => {
+            set_fullscreen(true);
+            next_frame().await;
 
-    let mut hamiltonian_graphs = Vec::with_capacity(NODES_N);
-    let mut non_hamiltonian_graphs = Vec::with_capacity(EDGES_N);
+            let mut nodes = Vec::with_capacity(NODES_N);
+            let gap = (2.0 * PI) / NODES_N as f32;
 
-    let start = Instant::now();
-
-    loop {
-        let mut graph = Graph::new(NODES_N, EDGES_N);
-        graph.safety();
-        graph.generate(&mut rng);
-
-        if graph.is_hamiltonian() {
-            if hamiltonian_graphs.len() < GRAPHS_N {
-                hamiltonian_graphs.push(graph.edges);
+            for i in 0..NODES_N {
+                let x = GRAPH_RADIUS * (i as f32 * gap).cos() + screen_width() / 2.0;
+                let y = GRAPH_RADIUS * (i as f32 * gap).sin() + screen_height() / 2.0;
+                nodes.push(vec2(x, y))
             }
-        } else {
-            if non_hamiltonian_graphs.len() < GRAPHS_N {
-                non_hamiltonian_graphs.push(graph.edges);
+
+            let mut graph = Graph::new(NODES_N);
+            graph.safety();
+
+            let mut kind = GraphKind::Hamiltonian;
+
+            let mut cycle = graph.get_cycle();
+
+            loop {
+                if is_key_pressed(KeyCode::R) {
+                    graph.generate_with_given_kind(kind);
+
+                    kind = match kind {
+                        GraphKind::Hamiltonian => GraphKind::NonHamiltonian,
+                        GraphKind::NonHamiltonian => GraphKind::Hamiltonian,
+                    };
+
+                    cycle = graph.get_cycle();
+                }
+
+                for node_pos in &nodes {
+                    draw_circle(node_pos.x, node_pos.y, NODE_RADIUS, WHITE);
+                }
+
+                for i in 0..NODES_N {
+                    for j in i + 1..NODES_N {
+                        if graph.edges[i][j] {
+                            draw_line(nodes[i].x, nodes[i].y, nodes[j].x, nodes[j].y, 5.0, WHITE);
+                        }
+                    }
+                }
+
+                if !cycle.is_empty() && is_key_down(KeyCode::E) {
+                    for i in 0..NODES_N {
+                        let node = cycle[i];
+                        let next_node = cycle[i + 1];
+
+                        draw_line(
+                            nodes[node].x,
+                            nodes[node].y,
+                            nodes[next_node].x,
+                            nodes[next_node].y,
+                            15.0,
+                            DARKGREEN,
+                        );
+                    }
+                }
+
+                next_frame().await
             }
         }
+        Mode::Generation => {
+            let mut hamiltonian_graphs = Vec::new();
+            let mut non_hamiltonian_graphs = Vec::new();
 
-        if hamiltonian_graphs.len() == GRAPHS_N && non_hamiltonian_graphs.len() == GRAPHS_N {
-            break;
+            let start = Instant::now();
+
+            loop {
+                let mut graph = Graph::new(NODES_N);
+                graph.safety();
+                graph.generate();
+
+                if !graph.get_cycle().is_empty() {
+                    if hamiltonian_graphs.len() < GRAPHS_N {
+                        hamiltonian_graphs.push(graph.edges);
+                    }
+                } else if non_hamiltonian_graphs.len() < GRAPHS_N {
+                    non_hamiltonian_graphs.push(graph.edges);
+                }
+
+                if hamiltonian_graphs.len() == GRAPHS_N && non_hamiltonian_graphs.len() == GRAPHS_N
+                {
+                    break;
+                }
+            }
+
+            println!("{:?}", start.elapsed().as_secs_f32());
+
+            for (graphs, name) in [
+                (hamiltonian_graphs, "hamiltonian_graphs"),
+                (non_hamiltonian_graphs, "non_hamiltonian_graphs"),
+            ] {
+                let content = Content {
+                    info: Info {
+                        nodes_n: NODES_N,
+                        graphs_n: GRAPHS_N,
+                    },
+                    graphs: graphs.clone(),
+                };
+
+                std::fs::write(
+                    format!("{}.json", name),
+                    serde_json::to_string_pretty(&content).unwrap(),
+                )
+                .unwrap()
+            }
         }
-    }
-
-    println!("{:?}", start.elapsed().as_secs_f32());
-
-    for (graphs, name) in [
-        (hamiltonian_graphs, "hamiltonian_graphs"),
-        (non_hamiltonian_graphs, "non_hamiltonian_graphs"),
-    ] {
-        let content = Content {
-            info: Info {
-                nodes_n: NODES_N,
-                edges_n: EDGES_N,
-                graphs_n: GRAPHS_N,
-            },
-            graphs: graphs.clone(),
-        };
-
-        std::fs::write(
-            format!("{}.json", name),
-            serde_json::to_string_pretty(&content).unwrap(),
-        )
-        .unwrap()
     }
 }
